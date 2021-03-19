e7e103ab6576dd00e766c0309b7eb61c7be0501b
charss <charleskylereyes16@gmail.com>
2021-03-19 11:10:06 +0800

    Initialized repo

---
+++ DogRegister.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <link rel='stylesheet' href='./style.css'>
+    </head>
+    <body>
+    <form>
+        <div>
+            <label>Name<label><br>
+            <input type='text' name='name'/><br>
+            <label>Breed<label><br>
+            <input type='text' name='breed'/><br>
+            <label>Age<label><br>
+            <input type='text' name='age'/><br>
+            <label>Address<label><br>
+            <input type='text' name='address'/><br>
+            <label>Color<label><br>
+            <input type='text' name='color'/><br>
+            <label>Height<label><br>
+            <input type='text' name='height'/><br>
+            <label>Weight<label><br>
+            <input type='text' name='weight'/><br>
+            <input type='submit' name='submit'>
+        </div>
+        
+    </form>
+    </body>
+</html>
